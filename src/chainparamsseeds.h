8b282bd3381b3c549a609c8e4a2888b34c063c51
odavila466 <81089276+odavila466@users.noreply.github.com>
2021-05-10 10:53:34 -0400

    Add files via upload

---
+++ src/chainparamsseeds.h
@@ -0,0 +1,15 @@
+#ifndef Kron_CHAINPARAMSSEEDS_H
+#define Kron_CHAINPARAMSSEEDS_H
+/**
+ * Each line contains a 16-byte IPv6 address and a port.
+ * IPv4 as well as onion addresses are wrapped inside a IPv6 address accordingly.
+ */
+static SeedSpec6 pnSeed6_main[] = {
+   
+};
+
+
+static SeedSpec6 pnSeed6_test[] = {
+    
+};
+#endif // Kron_CHAINPARAMSSEEDS_H
