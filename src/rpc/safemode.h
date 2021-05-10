e8e029c5c8f5b48a1b5fe03ee4335e06aeffb2b2
odavila466 <81089276+odavila466@users.noreply.github.com>
2021-05-10 10:42:18 -0400

    Add files via upload

---
+++ src/rpc/safemode.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2017 The Bitcoin Core developers
+// Copyright (c) 2017-2019 The Kron Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef Kron_RPC_SAFEMODE_H
+#define Kron_RPC_SAFEMODE_H
+
+static const bool DEFAULT_DISABLE_SAFEMODE = true;
+
+void ObserveSafeMode();
+
+#endif // Kron_RPC_SAFEMODE_H
